7aebc748dc5267cd84a88c37b9e08acf6c845172
Ana Luiza <analuizasantosnascimento4@gmail.com>
2025-10-24 11:14:19 -0300

    conexao com db ✔

---
+++ back-end/conexao.py
@@ -0,0 +1,25 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os 
+
+# carregar .env
+load_dotenv()
+
+
+def conector():
+    try:
+        conexao = mysql.connector.connect(
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+        cursor = conexao.cursor()
+        print("conexão estabelecida")
+        return conexao,cursor
+    except Exception as erro:
+        print(f"Erro de conexao: {erro}")
+        return None, None
+    
+conector()    
